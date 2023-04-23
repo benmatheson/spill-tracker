351e55572cd4708ff8357390534694abff84bff9
Ben Matheson <benmatheson@Bens-M2-MBP.hitronhub.home>
2023-04-22 16:24:09 -0800

    download script

---
+++ spill-data-download.R
@@ -0,0 +1,39 @@
+
+
+install.packages('readr')
+install.packages('dplyr')
+install.packages('jsonlite')
+install.packages('janitor')
+install.packages('lubridate')
+
+
+
+library(readr)
+library(dplyr)
+library(jsonlite)
+library(janitor)
+library(lubridate)
+
+# setwd("projects/spill-tracker")
+# getwd()
+
+# https://dec.alaska.gov/Applications/SPAR/PublicMVC/PERP/GetSearchedSpills?spillDateStart=01%2F01%2F2023%2000%3A00%3A00&spillDateEnd=04%2F22%2F2023%2000%3A00%3A00
+
+
+url <- "https://dec.alaska.gov/Applications/SPAR/PublicMVC/PERP/GetSearchedSpills?spillDateStart=01%2F01%2F2023%2000%3A00%3A00&spillDateEnd=12%231%2F2023%2000%3A00%3A00"
+
+
+data_raw <- read_csv(url)
+
+data <- data_raw %>% clean_names()
+  
+  
+today <- today()
+
+    
+write_json(data, paste0("data/spill_data_",today,".json"))
+
+
+
+
+
